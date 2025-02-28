d5a02959e2fa9de865d1df8eb4a4c6a59e156a39
Paul Freire <paul.freire@crifa.com>
2025-02-28 16:35:03 -0500

    Feat: Implementacion de reportes usando Mochawesome

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,33 @@
-const { defineConfig } = require("cypress");
+// const { defineConfig } = require("cypress");
 
+// module.exports = defineConfig({
+//   viewportHeight: 1080,
+//   viewportWidth: 1920,
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+
+//     baseUrl: 'https://www.demoblaze.com',
+//     specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}'
+//   },
+// });
+
+
+const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'mochawesome-report',
+    overwrite: false,
+    html: true,
+    json: false
+  },
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-
+    // ... otras configuraciones e2e ...
     baseUrl: 'https://www.demoblaze.com',
     specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}'
-  },
+  }
 });
